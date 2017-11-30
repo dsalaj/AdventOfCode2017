9abd59b98d259765f3681ce5a7d8117f5a9173e1
Darjan Salaj <salaj@gmx.at>
2017-12-01 00:57:50 +0100

    day 1.2 python

--- 1_solution.py
+++ 1_solution.py
@@ -3,6 +3,12 @@ with open('1_input.txt') as file:
     data = list(data)
     data = [int(c) for c in data[:-1]]
 
-target_nums = [data[-i-1] if data[-i-1] == data[-i] else 0
-               for i in range(len(data))]
-print(sum(target_nums))
+full = len(data)
+target_nums = [data[i] if data[(i+1) % full] == data[i] else 0
+               for i in range(full)]
+print(sum(target_nums))  # solution 1
+
+half = full // 2
+target_nums = [data[i] if data[(i+half) % full] == data[i] else 0
+               for i in range(full)]
+print(sum(target_nums))  # solution 2
