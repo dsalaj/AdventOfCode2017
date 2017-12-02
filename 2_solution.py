25d90209fc29176e2a55146ff1b2d34427d3a9b6
Darjan Salaj <salaj@gmx.at>
2017-12-02 01:05:21 +0100

    day 2.2 python

--- 2_solution.py
+++ 2_solution.py
@@ -2,8 +2,23 @@ with open('2_input.txt') as file:
     data = file.readlines()
 data = [l.rstrip().split('\t') for l in data]
 data = [[int(cell) for cell in row] for row in data]
+
 max_rows = [max(row) for row in data]
 min_rows = [min(row) for row in data]
 chk_rows = [max_rows[i] - min_rows[i] for i in range(len(data))]
 chksum = sum(chk_rows)
-print(chksum)
+print(chksum)  # solution 1
+
+divs = []
+for row in data:
+    row_div = None
+    for i in row:
+        for j in row:
+            if i != j:
+                if i % j == 0:
+                    row_div = i / j
+                    break
+        if row_div is not None:
+            break
+    divs.append(row_div)
+print(int(sum(divs)))  # solution 2
